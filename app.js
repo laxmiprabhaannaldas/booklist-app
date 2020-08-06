207d42d1abcb41a7f9e50d625af74cd938dee8b0
laxmiprabhaannaldas <annaldaslaxmiprabha@gmail.com>
2020-08-06 16:44:42 +0530

    Validated the form and used custom alert using bootstrap

--- app.js
+++ app.js
@@ -43,6 +43,17 @@ class UI {
             el.parentElement.parentElement.remove();
         }
     }
+    static showAlert(message, className) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div, form);
+
+        //vanish in 3 seconds
+        setTimeout(() => document.querySelector('.alert').remove(), 3000);
+    }
     static clearFields() {
         document.querySelector('#title').value = '';
         document.querySelector('#author').value = '';
@@ -62,17 +73,28 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     const author = document.querySelector('#author').value;
     const isbn = document.querySelector('#isbn').value;
 
-    //Instantiate book
-    const book = new Book(title, author, isbn);
-    console.log(book);
+    //validate 
+    if (title === "" || author === "" || isbn === "") {
+        UI.showAlert('Pls!! fill all fields', 'danger');
+    } else {
+        //Instantiate book
+        const book = new Book(title, author, isbn);
+        console.log(book);
 
-    // Add Book to UI
-    UI.addBookToList(book);
+        // Add Book to UI
+        UI.addBookToList(book);
 
-    // Clear fields
-    UI.clearFields();
+        // Show success message
+        UI.showAlert('Book Added', 'success');
+
+        // Clear fields
+        UI.clearFields();
+    }
+
 });
 // Event: Remove a Book
 document.querySelector("#book-list").addEventListener('click', (e) => {
     UI.deleteBook(e.target)
+        // Show success message
+    UI.showAlert('Book Removed', 'success');
 });
