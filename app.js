a55e99c6af35e9ed01ef3066d2101c4ee4047deb
laxmiprabhaannaldas <annaldaslaxmiprabha@gmail.com>
2020-08-06 16:09:49 +0530

    The form information added to class and displayed it in console

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ class UI {
         const books = StoredBooks;
         books.forEach((book) => UI.addBookToList(book));
     }
-    static addBookToList() {
+    static addBookToList(book) {
         const list = document.querySelector('#book-list');
 
         const row = document.createElement('tr');
@@ -41,5 +41,21 @@ class UI {
 }
 //Store class:Handles Storage
 // Event: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
 // Event: Add a Book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+    // Prevent actual submit
+    e.preventDefault();
+
+    //get form values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    //Instantiate book
+    const book = new Book(title, author, isbn);
+    console.log(book);
+
+
+});
 // Event: Remove a Book
